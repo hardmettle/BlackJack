e2a9da829b44321da85acd31c471925b752ecf89
Harsh Gupta <harsh2005_7@yahoo.com>
2017-01-15 15:32:21 +0530

    Changes in crooked deal and main method cleanup.Changed gameplay based on probability.

--- src/main/scala/com/main/MainGame.scala
+++ src/main/scala/com/main/MainGame.scala
@@ -16,7 +16,7 @@ object MainGame {
       if (aceValue.toLowerCase() == "x") {
         println("Thanks for playing. Bye !")
         System.exit(0)
-      } else if (aceValue.toCharArray.head != '1' || (aceValue.length == 2 && aceValue.toCharArray.head != '1' && aceValue.toCharArray.tail.head !=1) ) {
+      } else if (!aceValue.equals("11") && !aceValue.equals("1")) {
         println("Please enter either 1 or 11. Use x|X to exit. ")
       } else {
         println("Loading game... ")
@@ -24,53 +24,14 @@ object MainGame {
         val init2 = init.updateGameStatus(DEAL("Dealer"),init)
         var game = init2.updateGameStatus(DEAL("Player"),init2)
         val r = scala.util.Random
-        println(s"Game loaded.One of the card dealer has ${game.dealer.cards(r.nextInt(2))}")
+        val dealerCard = game.dealer.cards(r.nextInt(2))
+        println(s"Game loaded.One of the card dealer has $dealerCard")
         println(s"You have :")
         game.player.cards.foreach(c => println(c.name + " of " +c.value))
         var numberOfHitsRemaining = 5
-        println(s"Make your choice. You have $numberOfHitsRemaining number of hits remaining")
-        println("Press h to HIT or s to STAND")
-        while(true){
-          val choice = scala.io.StdIn.readLine().trim.toLowerCase()
-          if (choice.length > 1) println("Please enter correct option.")
-          if(choice.charAt(0) != 'x'){
-            val h = choice.charAt(0) - 104
-            val s =choice.charAt(0) - 115
-            if(h != 0 && s != 0){
-              println(s"h is $h")
-              println(s"s is $s")
-              println("Please choose the right option h|s|x ")
-            }else{
-              if(choice.charAt(0) == 'h' && numberOfHitsRemaining == 0){
-                println("Sorry you have no hits remaining.")
-                println(s"Dealer has :")
-                game.dealer.cards.foreach(c => println(c.name + " of " +c.value))
-                println(s"You have :")
-                game.player.cards.foreach(c => println(c.name + " of " +c.value))
-                println("The final outcome is : ")
-                game.updateGameStatus(STAND(),game)
-                System.exit(0)
-              }else if(h == 0){
-                game = game.updateGameStatus(HIT(None),game)
-                numberOfHitsRemaining = numberOfHitsRemaining - 1
-                println(s"You have ")
-                game.player.cards.foreach(c => println(s"${c.name} of ${c.value}"))
-                println(s"Make your next move. You have $numberOfHitsRemaining number of hits remaining")
-              }else{
-                println(s"Dealer has : ")
-                game.dealer.cards.foreach(c => println(c.name + " of " +c.value))
-                println(s"You have :")
-                game.player.cards.foreach(c => println(c.name + " of " +c.value))
-                println("The final outcome is : ")
-                game.updateGameStatus(STAND(),game)
-                System.exit(0)
-              }
-            }
-          }else{
-            println("Thanks for playing. Bye !")
-            System.exit(0)
-          }
-        }
+        println(s"Make your choice HIT or STAND. You have $numberOfHitsRemaining number of hits remaining")
+        game.gamePlay(numberOfHitsRemaining)
+        System.exit(0)
       }
     }
   }
--- src/main/scala/com/members/Members.scala
+++ src/main/scala/com/members/Members.scala
@@ -13,9 +13,29 @@ case class SHOW_VALUE() extends Instruction
 case class UPDATE_DECK(currentCards:List[Card],instruction: Instruction) extends Instruction
 case class UPDATE_PLAYER(newCardOne:Card,newCardTwo:Card) extends Instruction
 
+import com.members
+
 import scala.util.Random
 case class BlackJack(player: Player,dealer:Player,cardDeck: CardDeck){
 
+  def gamePlay(hitsRemaining:Int) = {
+    this.player.makeAMove(hitsRemaining,this.cardDeck.cards) match {
+      case h@HIT(_) => {
+        this.updateGameStatus(h,this)
+        println(s"Player has chosen HIT. Now he has :")
+        this.player.cards.foreach(c => println(s"${c.name} of ${c.value}"))
+        this.player.makeAMove(hitsRemaining-1,this.cardDeck.cards)
+      }
+      case STAND() => {
+        println(s"Dealer has : ")
+        this.dealer.cards.foreach(c => println(c.name + " of " +c.value))
+        println(s"You have :")
+        this.player.cards.foreach(c => println(c.name + " of " +c.value))
+        println("The final outcome is : ")
+        this.updateGameStatus(STAND(),this)
+      }
+    }
+  }
   def updateGameStatus(instruction: Instruction,blackJack: BlackJack):BlackJack = {
     instruction match {
       case h@HIT(_) => this.hitCard()
@@ -30,22 +50,32 @@ case class BlackJack(player: Player,dealer:Player,cardDeck: CardDeck){
   }
 
   private def hitCard():BlackJack = {
-    val shuffledCards = Random.shuffle(this.cardDeck.cards)
-    val hitValue = shuffledCards.head
+    val currentCards = this.cardDeck.cards
+    val hitValue = currentCards.head
     val optionHitValue = Some(hitValue)
     val updatedPlayer = this.player.action(HIT(optionHitValue))
-    val updatedDeck = this.cardDeck.action(UPDATE_DECK(shuffledCards,HIT(optionHitValue)))
+    val updatedDeck = this.cardDeck.action(UPDATE_DECK(currentCards,HIT(optionHitValue)))
     this.copy(player = updatedPlayer,cardDeck = updatedDeck)
   }
   private def dealCard(deal: DEAL):BlackJack = {
     def crookedDeal():(Card,Card,List[Card]) = {
+      val cardsWithNine = this.cardDeck.cards.filter(c => c.value == 9)
       val cardsWithTen = this.cardDeck.cards.filter(c => c.value == 10)
-      val restOfTheCards = this.cardDeck.cards.filterNot(c => c.value == 10)
-      val shuffled = Random.shuffle(cardsWithTen)
-      val firstCard = shuffled.head
-      val shuffledTwo = Random.shuffle(shuffled.tail)
-      val secondCard = shuffledTwo.head
-      (firstCard,secondCard,List(firstCard,secondCard) ++ Random.shuffle(Random.shuffle(shuffledTwo) ++ restOfTheCards))
+      val cardsWithEleven = this.cardDeck.cards.filter(c => c.value == 11)
+      val restOfTheCards = this.cardDeck.cards.filterNot(c => c.value == 11 || c.value == 10 || c.value == 9)
+      val possibleCards = cardsWithNine ++ cardsWithTen ++ cardsWithEleven
+      val r = scala.util.Random
+      val randomIndex = r.nextInt(possibleCards.size)
+      val randomSelection = possibleCards(randomIndex)
+      val remainingTarget = 20 - randomSelection.value
+      val remainingPossibleCards = possibleCards.take(randomIndex) ++ possibleCards.drop(randomIndex+1)
+      val possibleCardsTwo = remainingPossibleCards.filter(c => c.value - remainingTarget == 0)
+      val randomIndexTwo = r.nextInt(possibleCardsTwo.size)
+      val randomSelectionTwo = possibleCardsTwo(randomIndexTwo)
+      val unUsedRemainingDeck = remainingPossibleCards.filterNot(c => randomSelectionTwo == c)
+
+      (randomSelection,randomSelectionTwo,List(randomSelection,randomSelectionTwo) ++
+        Random.shuffle(unUsedRemainingDeck ++ restOfTheCards))
     }
     val (updatedPlayer,shuffledCards) = if(deal.name == "Player"){
       val honestShuffledCards = Random.shuffle(this.cardDeck.cards)
@@ -72,6 +102,38 @@ case class Player(name:String,cards:List[Card]) extends Members{
   override def action(instruction: Instruction):Player = instruction match {
     case HIT(c) => this.copy(cards = cards :+ c.get)
     case UPDATE_PLAYER(one,two) => this.copy(cards=List(one,two))
+  }
+  def makeAMove(hitsRemaining:Int,cardsInDeck:List[Card]):Instruction = {
+    if(hitsRemaining != 0){
+      if(getProbablityForNextMove(hitsRemaining,cardsInDeck) > 0.5){
+        members.HIT(None)
+      }else{
+        STAND()
+      }
+    } else {
+      members.STAND()
+    }
+  }
+  private def getProbablityForNextMove(hitsRemaining:Int,cardsInDeck:List[Card]): Double = {
+    val total = this.cards.map(_.value).sum
+    def powerSet(t: List[Int]): List[List[Int]] = {
+      @annotation.tailrec
+      def pwr(t: List[Int], ps: List[List[Int]]): List[List[Int]] =
+        if (t.isEmpty) ps
+        else pwr(t.tail, ps ++ (ps map (_ :+ t.head) ))
+      pwr(t, List(List.empty[Int]))
+    }
+    val allCombinations = powerSet(cardsInDeck.map(_.value))//sample
+    val remainingSum = 21 - total
+    val favourableChoices = (1 to hitsRemaining).map(hits => {
+      allCombinations.filter(_.size == hits).map(_.sum).count(_ == remainingSum)
+    })//choices per hits
+    val allCombinationSize = allCombinations.size
+    val probability = favourableChoices.map(_.toDouble).reduce((f1,f2) => {
+      f1.toDouble / allCombinationSize.toDouble + f2.toDouble / allCombinationSize.toDouble
+    })
+    println(s"Probability is $probability")
+    probability
   }
 }
 
